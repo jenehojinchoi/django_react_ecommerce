d2b4571eb5dcf473593ba2cd285429a9e6a09f33
Jene Hojin Choi <jenehojinchoi@gmail.com>
2021-06-16 00:12:26 +0900

    feat: homepage, productpage

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/product/:id' component={ProductPage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
